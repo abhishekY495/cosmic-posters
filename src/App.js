ffb8b36bcc66f99ff1fea632537e641e0d0ac64d
Abhishek <67749693+abhishekY495@users.noreply.github.com>
2023-05-20 21:08:03 +0530

    Added elements to Route

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import { Route, Routes } from "react-router-dom";
 
 import HomePage from "./pages/HomePage/HomePage";
-import PostersListing from "./pages/PostersListing/PostersListing";
 import Navbar from "./components/Navbar/Navbar";
 import Footer from "./components/Footer/Footer";
+import PostersListing from "./pages/PostersListing/PostersListing";
+import Poster from "./pages/Poster/Poster";
+import SignUp from "./pages/SignUp/SignUp";
+import Login from "./pages/Login/Login";
+import Profile from "./pages/Profile/Profile";
+import Wishlist from "./pages/Wishlist/Wishlist";
+import Cart from "./pages/Cart/Cart";
 import "./App.css";
 
 function App() {
@@ -13,12 +19,12 @@ function App() {
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/posters-listing" element={<PostersListing />} />
-        <Route path="/poster/:id" />
-        <Route path="/signup" />
-        <Route path="/login" />
-        <Route path="/profile" />
-        <Route path="/wishlist" />
-        <Route path="/cart" />
+        <Route path="/poster/:id" element={<Poster />}/>
+        <Route path="/signup" element={<SignUp />}/>
+        <Route path="/login" element={<Login />} />
+        <Route path="/profile" element={<Profile />}/>
+        <Route path="/wishlist" element={<Wishlist />} />
+        <Route path="/cart" element={<Cart />} />
       </Routes>
       <Footer />
     </div>
