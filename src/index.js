252a6a0d9cce54eaba61ad30e71b0e1e0c306840
Abhishek <67749693+abhishekY495@users.noreply.github.com>
2023-05-20 18:56:10 +0530

    Added DataContext and Router

--- src/index.js
+++ src/index.js
@@ -1,10 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { BrowserRouter as Router } from "react-router-dom";
+
+import { DataContextProvider } from "./contexts/DataContext";
 import App from "./App";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <Router>
+      <DataContextProvider>
+        <App />
+      </DataContextProvider>
+    </Router>
   </React.StrictMode>
 );
