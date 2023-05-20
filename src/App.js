95d1cf7ae7e52e81f3fa0470655ba59e49308e19
Abhishek <67749693+abhishekY495@users.noreply.github.com>
2023-05-20 18:55:13 +0530

    Added route elements

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import { Route, Routes } from "react-router-dom";
 
+import HomePage from "./pages/HomePage/HomePage";
+import PostersListing from "./pages/PostersListing/PostersListing";
+import Navbar from "./components/Navbar/Navbar";
+import Footer from "./components/Footer/Footer";
+import "./App.css";
+
 function App() {
   return (
     <div className="App">
+      <Navbar />
       <Routes>
-        <Route path="/" />
-        <Route path="/posters-listing" />
+        <Route path="/" element={<HomePage />} />
+        <Route path="/posters-listing" element={<PostersListing />} />
         <Route path="/poster/:id" />
         <Route path="/signup" />
         <Route path="/login" />
@@ -13,6 +20,7 @@ function App() {
         <Route path="/wishlist" />
         <Route path="/cart" />
       </Routes>
+      <Footer />
     </div>
   );
 }
