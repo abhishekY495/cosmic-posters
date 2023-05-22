f10ce5f78cb568df71b023587f582e14030552c8
Abhishek <67749693+abhishekY495@users.noreply.github.com>
2023-05-22 23:19:30 +0530

    Refractor

--- src/pages/PostersListing/PostersListing.jsx
+++ src/pages/PostersListing/PostersListing.jsx
@@ -10,25 +10,25 @@ export default function PostersListing() {
 
   return (
     <>
-    <div id="poster-list-container">
-    <Filters />
-    <div id="poster-list">
-      {filteredPostersData.map(({ id, name, image, price, rating }) => {
-        return (
-          <div className="poster" key={id}>
-            <Link to={`/poster/${id}`}>
-              <img className="poster-image" src={image} alt={name} />
-            </Link>
-            <p className="poster-name">{name}</p>
-            <div className="price-rating">
-              <span>{price}</span>
-              <span>{rating}</span>
-            </div>
-          </div>
-        );
-      })}
-    </div>
-    </div>
+      <div id="posters-list-container">
+        <Filters />
+        <div id="posters-list">
+          {filteredPostersData.map(({ id, name, image, price, rating }) => {
+            return (
+              <div className="poster" key={id}>
+                <Link to={`/poster/${id}`}>
+                  <img className="poster-image" src={image} alt={name} />
+                </Link>
+                <p className="poster-name">{name}</p>
+                <div className="price-rating">
+                  <span>{price}</span>
+                  <span>{rating}</span>
+                </div>
+              </div>
+            );
+          })}
+        </div>
+      </div>
     </>
   );
 }
