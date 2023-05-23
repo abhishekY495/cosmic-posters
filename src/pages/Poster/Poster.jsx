f63c2712ea1f0d362f3c18f8788801abb50a5c9e
Abhishek <67749693+abhishekY495@users.noreply.github.com>
2023-05-23 20:32:50 +0530

    Added Poster data and basic style

--- src/pages/Poster/Poster.jsx
+++ src/pages/Poster/Poster.jsx
@@ -1,7 +1,26 @@
-import React from 'react'
+import React, { useContext } from "react";
+import { useParams } from "react-router-dom";
+
+import { DataContext } from "../../contexts/DataContext";
+import "./Poster.css"
 
 export default function Poster() {
+  const { postersData } = useContext(DataContext);
+  const { id } = useParams();
+
+  const poster = postersData.find((poster) => poster.id === id);
+
   return (
-    <div>Poster</div>
-  )
+    <div id="poster">
+      <img id="poster-image" src={poster?.image} alt={poster.name} />
+      <div id="poster-info">
+        <h2 id="poster-name">{poster?.name}</h2>
+        <p id="poster-rating">{poster.rating}</p>
+        <p id="poster-price">₹{poster.price}</p>
+        <p id="poster-description">{poster.description}</p>
+        <button>Add to Cart</button>
+        <button>Add to Wishlist</button>
+      </div>
+    </div>
+  );
 }
