48f088df73bd575bf4f8d912c8124fd90d920b16
Abhishek <67749693+abhishekY495@users.noreply.github.com>
2023-05-24 15:40:15 +0530

    Created basic UI

--- src/pages/Cart/Cart.jsx
+++ src/pages/Cart/Cart.jsx
@@ -1,7 +1,65 @@
-import React from 'react'
+import React, { useContext } from "react";
+
+import { CartContext } from "../../contexts/CartContext";
+import "./Cart.css";
 
 export default function Cart() {
+  const {
+    state: { cart },
+    dispatch,
+  } = useContext(CartContext);
+
+  const total = cart.reduce(
+    (acc, curr) => (acc += curr.price * curr.quantity),
+    0
+  );
+
+  const cartQuantityHandler = (e, poster) => {
+    const value = e.target.value;
+    dispatch({ type: "UPDATE_CART_QUANTITY", payload: { value, poster } });
+  };
+
+  const removeFromCartBtnHandler = (id) => {
+    dispatch({ type: "REMOVE_FROM_CART", payload: id });
+  };
+
   return (
-    <div>Cart</div>
-  )
+    <div id="cart-posters">
+      {cart.length === 0 && <p>Cart is Empty...</p> }
+      {cart.map((poster) => {
+        const { id, name, price, quantity } = poster;
+        return (
+          <div key={id} className="cart-poster">
+            <div className="cart-name-price">
+              <p>{name}</p>
+              <p>₹{price}</p>
+            </div>
+            <div className="cart-btn-group">
+              <button
+                onClick={(e) => cartQuantityHandler(e, poster)}
+                value="decrease"
+              >
+                -
+              </button>
+              <p>{quantity}</p>
+              <button
+                onClick={(e) => cartQuantityHandler(e, poster)}
+                value="increase"
+              >
+                +
+              </button>
+            </div>
+            <button onClick={() => removeFromCartBtnHandler(id)}>
+              Remove From Cart
+            </button>
+          </div>
+        );
+      })}
+      {total > 0 && (
+        <p>
+          Total - <b>{total.toFixed(2)}</b>
+        </p>
+      )}
+    </div>
+  );
 }
--- src/pages/Wishlist/Wishlist.jsx
+++ src/pages/Wishlist/Wishlist.jsx
@@ -1,7 +1,40 @@
-import React from 'react'
+import React, { useContext } from "react";
+
+import { WishlistContext } from "../../contexts/WishlistContext";
+import "./Wishlist.css";
 
 export default function Wishlist() {
+  const {
+    state: { wishlist },
+    dispatch,
+  } = useContext(WishlistContext);
+
+  const removeFromWishlistBtnHandler = (id) => {
+    dispatch({ type: "REMOVE_FROM_WISHLIST", payload: id });
+  };
+
   return (
-    <div>Wishlist</div>
-  )
+    <div id="wishlist-posters">
+      {wishlist.length === 0 && <p>Wishlist is Empty..</p> }
+      {wishlist.map((poster) => {
+        const { id, name, image, description, price, rating } = poster;
+        return (
+          <div key={id} className="wishlist-poster">
+            <img src={image} alt={name} className="wishlist-poster-image" />
+            <div className="wishlist-poster-info">
+              <p>{name}</p>
+              <div>
+                <p>{price}</p>
+                <p>{rating}</p>
+              </div>
+              <p>{description}</p>
+              <button onClick={() => removeFromWishlistBtnHandler(id)}>
+                Remove from Wishlist
+              </button>
+            </div>
+          </div>
+        );
+      })}
+    </div>
+  );
 }
