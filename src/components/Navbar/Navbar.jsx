b19cacff5220b8d2d94b69e0c5b8783974d7a188
Abhishek <67749693+abhishekY495@users.noreply.github.com>
2023-05-24 15:41:25 +0530

    Added cart and wishlist count

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,10 +1,19 @@
-import React from "react";
+import React, { useContext } from "react";
 import { Link } from "react-router-dom";
 
 import SearchBar from "../SearchBar/SearchBar";
+import { CartContext } from "../../contexts/CartContext";
+import { WishlistContext } from "../../contexts/WishlistContext";
 import "./Navbar.css";
 
 export default function Navbar() {
+  const {
+    state: { cart },
+  } = useContext(CartContext);
+  const {
+    state: { wishlist },
+  } = useContext(WishlistContext);
+
   return (
     <nav>
       <Link className="nav-links" to="/">
@@ -15,10 +24,10 @@ export default function Navbar() {
         Explore
       </Link>
       <Link className="nav-links" to="wishlist">
-        Wishlist
+        Wishlist <sup>{wishlist.length}</sup>
       </Link>
       <Link className="nav-links" to="cart">
-        Cart
+        Cart <sup>{cart.length}</sup>
       </Link>
       <Link className="nav-links" to="profile">
         Profile
