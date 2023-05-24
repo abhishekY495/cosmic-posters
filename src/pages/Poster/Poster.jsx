2beee2d0c459a29323718eb85b0dff2029663f0c
Abhishek <67749693+abhishekY495@users.noreply.github.com>
2023-05-24 23:25:48 +0530

    Added cart and wishlist functionnality

--- src/pages/Poster/Poster.jsx
+++ src/pages/Poster/Poster.jsx
@@ -2,24 +2,63 @@ import React, { useContext } from "react";
 import { useParams } from "react-router-dom";
 
 import { DataContext } from "../../contexts/DataContext";
-import "./Poster.css"
+import { CartContext } from "../../contexts/CartContext";
+import { WishlistContext } from "../../contexts/WishlistContext";
+import "./Poster.css";
 
 export default function Poster() {
   const { postersData } = useContext(DataContext);
+  const {
+    state: { cart },
+    dispatch: cartDispatch,
+  } = useContext(CartContext);
+  const {
+    state: { wishlist },
+    dispatch: wishlistDispatch,
+  } = useContext(WishlistContext);
   const { id } = useParams();
-
   const poster = postersData.find((poster) => poster.id === id);
+
+  const addToCartBtnHandler = (poster) => {
+    cartDispatch({ type: "ADD_TO_CART", payload: poster });
+  };
+  const removeFromCartBtnHandler = (id) => {
+    cartDispatch({ type: "REMOVE_FROM_CART", payload: id });
+  };
+
+  const addToWishlistBtnHandler = () => {
+    wishlistDispatch({ type: "ADD_TO_WISHLIST", payload: poster });
+  };
+  const removeFromWishlistBtnHandler = (id) => {
+    wishlistDispatch({ type: "REMOVE_FROM_WISHLIST", payload: id });
+  };
 
   return (
     <div id="poster">
-      <img id="poster-image" src={poster?.image} alt={poster.name} />
+      <img id="poster-image" src={poster?.image} alt={poster?.name} />
       <div id="poster-info">
         <h2 id="poster-name">{poster?.name}</h2>
-        <p id="poster-rating">{poster.rating}</p>
-        <p id="poster-price">₹{poster.price}</p>
-        <p id="poster-description">{poster.description}</p>
-        <button>Add to Cart</button>
-        <button>Add to Wishlist</button>
+        <p id="poster-rating">{poster?.rating}</p>
+        <p id="poster-price">₹{poster?.price}</p>
+        <p id="poster-description">{poster?.description}</p>
+        {cart.find(({ id }) => id === poster.id) ? (
+          <button onClick={() => removeFromCartBtnHandler(poster.id)}>
+            Remove from Cart
+          </button>
+        ) : (
+          <button onClick={() => addToCartBtnHandler(poster)}>
+            Add to Cart
+          </button>
+        )}
+        {wishlist.find(({ id }) => id === poster.id) ? (
+          <button onClick={() => removeFromWishlistBtnHandler(poster.id)}>
+            Remove from Wishlist
+          </button>
+        ) : (
+          <button onClick={() => addToWishlistBtnHandler(poster)}>
+            Add to Wishlist
+          </button>
+        )}
       </div>
     </div>
   );
