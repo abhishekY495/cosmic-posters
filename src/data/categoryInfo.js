d334d27527ae2fc483d4a9d4bf84e41590e61c00
Abhishek <67749693+abhishekY495@users.noreply.github.com>
2023-05-29 21:07:48 +0530

    Created categoryInfo.js

---
+++ src/data/categoryInfo.js
@@ -0,0 +1,22 @@
+export const categoryInfo = [
+  {
+    name: "Planet",
+    description:
+      "Our solar system is home to 8 amazing planets, ranging from small rocky worlds to large and gaseous with extreme temperatures.",
+  },
+  {
+    name: "Moon",
+    description:
+      "Moons come in many shapes, sizes, and types. Few have atmospheres and even hidden oceans beneath their surfaces.",
+  },
+  {
+    name: "Galaxy",
+    description:
+      "Galaxies are a huge collection of gas, dust, and billions of stars and their solar systems, all held together by gravity.",
+  },
+  {
+    name: "Nebula",
+    description:
+      "A nebula is a giant cloud of dust and gas in space that come from the gas and dust thrown out by the explosion of a dying star.",
+  },
+];
