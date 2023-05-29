f149ca5d774f0c2606a0eb67518142762d80e863
Abhishek <67749693+abhishekY495@users.noreply.github.com>
2023-05-29 10:19:26 +0530

    Feat: Added checkout link

--- src/pages/Cart/Cart.jsx
+++ src/pages/Cart/Cart.jsx
@@ -1,6 +1,7 @@
 import React, { useContext } from "react";
 
 import { CartContext } from "../../contexts/CartContext";
+import { Link } from "react-router-dom";
 import "./Cart.css";
 
 export default function Cart() {
@@ -9,10 +10,9 @@ export default function Cart() {
     dispatch,
   } = useContext(CartContext);
 
-  const total = cart.reduce(
-    (acc, curr) => (acc += curr.price * curr.quantity),
-    0
-  );
+  const total = cart
+    .reduce((acc, curr) => (acc += curr.price * curr.quantity), 0)
+    .toFixed(2);
 
   const cartQuantityHandler = (e, poster) => {
     const value = e.target.value;
@@ -24,42 +24,60 @@ export default function Cart() {
   };
 
   return (
-    <div id="cart-posters">
-      {cart.length === 0 && <p>Cart is Empty...</p> }
-      {cart.map((poster) => {
-        const { id, name, price, quantity } = poster;
-        return (
-          <div key={id} className="cart-poster">
-            <div className="cart-name-price">
-              <p>{name}</p>
-              <p>₹{price}</p>
-            </div>
-            <div className="cart-btn-group">
-              <button
-                onClick={(e) => cartQuantityHandler(e, poster)}
-                value="decrease"
-              >
-                -
+    <div id="cart-checkout-container">
+      <div id="cart-posters">
+        {cart.length === 0 && <p>Cart is Empty...</p>}
+        {cart.map((poster) => {
+          const { id, name, price, quantity } = poster;
+          return (
+            <div key={id} className="cart-poster">
+              <div className="cart-name-price">
+                <p>{name}</p>
+                <p>₹{price}</p>
+              </div>
+              <div className="cart-btn-group">
+                <button
+                  onClick={(e) => cartQuantityHandler(e, poster)}
+                  value="decrease"
+                >
+                  -
+                </button>
+                <p>{quantity}</p>
+                <button
+                  onClick={(e) => cartQuantityHandler(e, poster)}
+                  value="increase"
+                >
+                  +
+                </button>
+              </div>
+              <button onClick={() => removeFromCartBtnHandler(id)}>
+                Remove From Cart
               </button>
-              <p>{quantity}</p>
-              <button
-                onClick={(e) => cartQuantityHandler(e, poster)}
-                value="increase"
-              >
-                +
-              </button>
             </div>
-            <button onClick={() => removeFromCartBtnHandler(id)}>
-              Remove From Cart
-            </button>
-          </div>
-        );
-      })}
-      {total > 0 && (
-        <p>
-          Total - <b>{total.toFixed(2)}</b>
-        </p>
-      )}
+          );
+        })}
+      </div>
+      <div id="checkout-container">
+        <div id="chekout-posters">
+          {cart.map((poster) => {
+            return (
+              <p key={poster.id}>
+                {poster.name} - {poster.quantity}X
+              </p>
+            );
+          })}
+        </div>
+        {total > 0 && (
+          <p>
+            Total - <b>{total}</b>
+          </p>
+        )}
+        {cart.length > 0 && (
+          <Link to="/checkout">
+            <p id="checkout-link">Checkout</p>
+          </Link>
+        )}
+      </div>
     </div>
   );
 }
