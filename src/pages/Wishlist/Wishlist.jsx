f48ce6c81224b009e90dd5bdee3a7a7715ad405d
Abhishek <67749693+abhishekY495@users.noreply.github.com>
2023-06-02 23:34:39 +0530

    Styling

--- src/pages/Wishlist/Wishlist.jsx
+++ src/pages/Wishlist/Wishlist.jsx
@@ -1,5 +1,5 @@
 import React, { useContext } from "react";
-import { useNavigate } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 
 import { WishlistContext } from "../../contexts/WishlistContext";
 import { CartContext } from "../../contexts/CartContext";
@@ -26,34 +26,71 @@ export default function Wishlist() {
   const goToCartBtnHandler = () => navigate("/cart");
 
   return (
-    <div id="wishlist-posters">
-      {wishlist.length === 0 && <p>Wishlist is Empty..</p>}
-      {wishlist.map((poster) => {
-        const { id, name, image, description, price, rating } = poster;
-        return (
-          <div key={id} className="wishlist-poster">
-            <img src={image} alt={name} className="wishlist-poster-image" />
-            <div className="wishlist-poster-info">
-              <p>{name}</p>
-              <div>
-                <p>{price}</p>
-                <p>{rating}</p>
+    <>
+      {wishlist.length === 0 ? (
+        <p className="wishlist-message-1">Wishlist is Empty...</p>
+      ) : (
+        <p className="wishlist-message-2">
+          {wishlist.length > 1
+            ? `${wishlist.length} Posters`
+            : `${wishlist.length} Poster`}
+        </p>
+      )}
+      <div
+        className={
+          wishlist.length === 1
+            ? "wishlist-posters-1"
+            : wishlist.length === 2
+            ? "wishlist-posters-2"
+            : "wishlist-posters-all"
+        }
+      >
+        {wishlist.map((poster) => {
+          const { id, name, image, price, rating } = poster;
+          return (
+            <div key={id} className="wishlist-poster-container">
+              <div className="wishlist-poster-image">
+                <Link to={`/poster/${id}`}>
+                  <img src={image} alt={name} />
+                </Link>
               </div>
-              <p>{description}</p>
-              {cart.find((poster) => poster.id === id) ? (
-                <button onClick={goToCartBtnHandler}>Go to Cart</button>
-              ) : (
-                <button onClick={() => addToCartBtnHandler(poster)}>
-                  Add to Cart
-                </button>
-              )}
-              <button onClick={() => removeFromWishlistBtnHandler(id)}>
-                Remove from Wishlist
-              </button>
+              <div className="wishlist-poster-info">
+                <p className="wishlist-poster-name">{name}</p>
+                <p className="wishlist-poster-price">₹{price}</p>
+                <p className="wishlist-poster-rating">
+                  <img
+                    className="star-icon"
+                    src="https://img.icons8.com/?size=22&id=8ggStxqyboK5&format=svg"
+                    alt="star"
+                  />
+                  {rating}
+                </p>
+                <img
+                  className="wishlist-poster-icon"
+                  onClick={() => removeFromWishlistBtnHandler(id)}
+                  src="https://img.icons8.com/?size=22&id=V4c6yYlvXtzy&format=svg"
+                  alt="red-heart"
+                />
+                {cart.find((poster) => poster.id === id) ? (
+                  <button
+                    className="wishlist-poster-add-to-cart-btn wishlist-poster-incart-btn"
+                    onClick={goToCartBtnHandler}
+                  >
+                    Go to Cart
+                  </button>
+                ) : (
+                  <button
+                    className="wishlist-poster-add-to-cart-btn"
+                    onClick={() => addToCartBtnHandler(poster)}
+                  >
+                    Add to Cart
+                  </button>
+                )}
+              </div>
             </div>
-          </div>
-        );
-      })}
-    </div>
+          );
+        })}
+      </div>
+    </>
   );
 }
