dc0baebd9e52f3a114b195d8f644bf89e7c65767
Abhishek <67749693+abhishekY495@users.noreply.github.com>
2023-06-05 17:12:16 +0530

    Refactor and Styling

--- src/pages/Checkout/Checkout.jsx
+++ src/pages/Checkout/Checkout.jsx
@@ -2,8 +2,6 @@ import React, { useContext, useState } from "react";
 
 import { CartContext } from "../../contexts/CartContext";
 import { AddressContext } from "../../contexts/AddressContext";
-import AddressListing from "../../components/Address/AddressListing/AddressListing";
-import AddressFormModal from "../../components/Address/AddressFormModal/AddressFormModal";
 import "./Checkout.css";
 
 export default function Checkout() {
@@ -13,43 +11,107 @@ export default function Checkout() {
   const {
     state: { addresses },
   } = useContext(AddressContext);
-  const [openModal, setOpenModal] = useState(false);
+  const [selectedAddress, setSelectedAddress] = useState({});
+  const [disabledPlaceOrderBtn, setDisabledPlaceOrderBtn] = useState(true);
 
   const total = cart
     .reduce((acc, curr) => (acc += curr.price * curr.quantity), 0)
     .toFixed(2);
 
-  const toggleAddressFormModal = () => setOpenModal(!openModal);
+  const setDelieveryAddress = (address) => {
+    setSelectedAddress(address);
+    setDisabledPlaceOrderBtn(false);
+  };
 
   return (
     <>
-      <AddressFormModal openModal={openModal} setOpenModal={setOpenModal} />
-      <div id="checkout">
-        {addresses.length === 0 && <p>No Addresses found...</p>}
-        <button onClick={toggleAddressFormModal}>Add Address</button>
-        {addresses.map((address) => {
-          return (
-            <label key={address.id}>
-              <input type="radio" name="address" />
-              <AddressListing address={address} checkout />
-            </label>
-          );
-        })}
-        {cart.length === 0 && <p>No items to checkout...</p>}
-        {cart.map((poster) => {
-          return (
-            <p key={poster.id}>
-              {poster.name} - {poster.quantity}X
-            </p>
-          );
-        })}
-        {total > 0 && (
-          <p>
-            Total - <b>{total}</b>
-          </p>
-        )}
-        <button>Place Order</button>
-      </div>
+      {cart.length === 0 ? (
+        <p id="checkout-empty-message">No items to checkout...</p>
+      ) : (
+        <>
+          <p id="checkout-heading">Checkout ({cart.length})</p>
+          <div id="address-checkout-container">
+            <div id="address-container">
+              {addresses.map((address) => {
+                const { id, name, street, city, pincode, state, mobile } =
+                  address;
+                return (
+                  <label key={id} className="single-address">
+                    <input
+                      type="radio"
+                      name="address"
+                      onChange={() => setDelieveryAddress(address)}
+                    />
+                    <div>
+                      <p className="address-name">{name}</p>
+                      <p className="address-street">{street}</p>
+                      <p className="address-city-pincode">
+                        {city}, {pincode}
+                      </p>
+                      <p className="address-state">{state}</p>
+                      <p className="address-mobile">Mobile: {mobile}</p>
+                    </div>
+                  </label>
+                );
+              })}
+            </div>
+            <div id="checkout-container">
+              <div>
+                {cart.map((poster) => {
+                  return (
+                    <div key={poster.id} className="checkout-container-posters">
+                      <p className="name-quantity">
+                        {poster.name} ({poster.quantity})
+                      </p>
+                      <p className="price-quantity">
+                        {(poster.quantity * poster.price).toFixed(2)}
+                      </p>
+                    </div>
+                  );
+                })}
+              </div>
+              {total > 0 && (
+                <div id="checkout-total-container">
+                  <div id="delivery-charge">
+                    <p>Delivery charge</p>
+                    <p>Free</p>
+                  </div>
+                  <div id="checkout-total-price">
+                    <p id="heading">Total</p>
+                    <p id="price">
+                      ₹<b>{total}</b>
+                    </p>
+                  </div>
+                </div>
+              )}
+              {selectedAddress.name ? (
+                <div id="delivery-address">
+                  <p className="address-name">{selectedAddress.name}</p>
+                  <p className="address-street">{selectedAddress.street}</p>
+                  <p className="address-city-pincode">
+                    {selectedAddress.city}, {selectedAddress.pincode}
+                  </p>
+                  <p className="address-state">{selectedAddress.state}</p>
+                  <p className="address-mobile">
+                    Mobile: {selectedAddress.mobile}
+                  </p>
+                </div>
+              ) : (
+                <p id="select-address-message">Select an Address</p>
+              )}
+              <button
+                id={`place-order-btn${
+                  disabledPlaceOrderBtn ? "-disabled" : ""
+                }`}
+                disabled={disabledPlaceOrderBtn}
+                onClick={() => console.log("Order placed")}
+              >
+                Place Order
+              </button>
+            </div>
+          </div>
+        </>
+      )}
     </>
   );
 }
