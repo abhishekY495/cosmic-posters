3f7dff9081f54f4810d5190ea9c4445b20ed3131
Abhishek <67749693+abhishekY495@users.noreply.github.com>
2023-06-05 13:13:59 +0530

    Refactor and Styling

--- src/components/Authentication/Authentication.jsx
+++ src/components/Authentication/Authentication.jsx
@@ -3,6 +3,7 @@ import { Link } from "react-router-dom";
 
 import { AuthContext } from "../../contexts/AuthContext";
 import { auth } from "../../config/firebase";
+import "./Authentication.css";
 
 export default function Authentication({ signup, login }) {
   const { signUpUser, loginUser, updateProfile } = useContext(AuthContext);
@@ -10,82 +11,106 @@ export default function Authentication({ signup, login }) {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [message, setMessage] = useState("");
-  const test = true;
+  const guestCredentials = {
+    email: "neilarmstrong@nasa.com",
+    password: "neil@123456",
+  };
 
   const signUpBtnHandler = async () => {
-    if (email.length === 0) {
-      console.log("Enter email...");
-    } else if (password.length === 0) {
-      console.log("Enter password...");
-    } else {
-      try {
-        await signUpUser(email, password);
-        await updateProfile(auth.currentUser, { displayName: name });
-        setMessage("You have Signed Up.");
-      } catch (error) {
-        console.log(error);
-      }
+    try {
+      await signUpUser(email, password);
+      await updateProfile(auth.currentUser, { displayName: name });
+      setMessage("You have Signed Up.");
+    } catch (error) {
+      console.log(error);
     }
   };
 
-  const loginBtnHandler = async (test) => {
-    if (test) {
-      try {
-        await loginUser("neilarmstrong@nasa.com", "neil@123456");
-        setMessage("You have Logged In.");
-      } catch (error) {
-        console.log(error);
-      }
-    } else if (email.length === 0) {
-      console.log("Enter email...");
-    } else if (password.length === 0) {
-      console.log("Enter password...");
-    } else {
-      try {
-        await loginUser(email, password);
-        setMessage("You have Logged In.");
-      } catch (error) {
-        console.log(error);
-      }
+  const loginBtnHandler = async () => {
+    try {
+      await loginUser(email, password);
+      setMessage("You have Logged In.");
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const formSubmitHandler = (e) => {
+    e.preventDefault();
+    if (signup) {
+      return signUpBtnHandler();
+    } else if (login) {
+      return loginBtnHandler();
+    }
+  };
+
+  const guestLogin = async () => {
+    setEmail(guestCredentials.email);
+    setPassword(guestCredentials.password);
+    try {
+      await loginUser(guestCredentials.email, guestCredentials.password);
+      setMessage("You have Logged In.");
+    } catch (error) {
+      console.log(error);
     }
   };
 
   return (
-    <div>
+    <form onSubmit={(e) => formSubmitHandler(e)} id="auth-form">
       {signup && (
         <label>
-          Name -
-          <input type="text" onChange={(e) => setName(e.target.value)} />
+          Name
+          <input
+            required
+            type="text"
+            onChange={(e) => setName(e.target.value)}
+          />
         </label>
       )}
       <label>
-        Email -
-        <input type="email" onChange={(e) => setEmail(e.target.value)} />
+        Email
+        <input
+          required
+          value={email}
+          type="email"
+          onChange={(e) => setEmail(e.target.value)}
+        />
       </label>
       <label>
-        Password -
-        <input type="password" onChange={(e) => setPassword(e.target.value)} />
+        Password
+        <input
+          required
+          value={password}
+          type="password"
+          onChange={(e) => setPassword(e.target.value)}
+        />
       </label>
       {signup && (
         <>
-          <button onClick={signUpBtnHandler}>Sign Up</button>
-          <p>
+          <button id="signup-btn" type="submit">
+            Sign Up
+          </button>
+          <p id="account-message">
             Have an Account? <Link to="/login">Login</Link>
           </p>
         </>
       )}
       {login && (
         <>
-          <button onClick={() => loginBtnHandler()}>Login</button>
-          <button onClick={() => loginBtnHandler(test)}>
-            Login with Test Credentials
-          </button>
-          <p>
+          <div id="login-btn-container">
+            <button id="login-btn" type="submit">
+              Login
+            </button>
+            <button id="guest-login-btn" type="button" onClick={guestLogin}>
+              Guest Login
+            </button>
+          </div>
+          <p id="account-message">
             Dont have an Account? <Link to="/signup">Sign Up</Link>
           </p>
         </>
       )}
       {message && <p>{message}</p>}
-    </div>
+    </form>
   );
 }
