bac2995671c199d082524d96f113099c2ca05cc2
Abhishek <67749693+abhishekY495@users.noreply.github.com>
2023-06-05 13:13:59 +0530

    Styling: fix scroll view

--- src/pages/Cart/Cart.jsx
+++ src/pages/Cart/Cart.jsx
@@ -32,81 +32,83 @@ export default function Cart() {
           {cart.length > 1 ? `${cart.length} Posters` : `${cart.length} Poster`}
         </p>
       )}
-      <div id="cart-checkout-container">
-        <div id="cart-posters-container">
-          {cart.map((poster) => {
-            const { id, image, name, price, quantity } = poster;
-            return (
-              <div key={id} className="cart-poster">
-                <Link
-                  className="cart-poster-image-container"
-                  to={`/poster/${id}`}
-                >
-                  <img src={image} className="cart-poster-image" alt={name} />
-                </Link>
-                <div className="cart-poster-info">
-                  <Link to={`/poster/${id}`}>
-                    <p className="cart-poster-name">{name}</p>
+      {cart.length > 0 && (
+        <div id="cart-checkout-container">
+          <div id="cart-posters-container">
+            {cart.map((poster) => {
+              const { id, image, name, price, quantity } = poster;
+              return (
+                <div key={id} className="cart-poster">
+                  <Link
+                    className="cart-poster-image-container"
+                    to={`/poster/${id}`}
+                  >
+                    <img src={image} className="cart-poster-image" alt={name} />
                   </Link>
-                  <p className="cart-poster-price">₹{price}</p>
-                  <div className="quantity-btn-group">
+                  <div className="cart-poster-info">
+                    <Link to={`/poster/${id}`}>
+                      <p className="cart-poster-name">{name}</p>
+                    </Link>
+                    <p className="cart-poster-price">₹{price}</p>
+                    <div className="quantity-btn-group">
+                      <button
+                        className="decrease-btn"
+                        onClick={(e) => cartQuantityHandler(e, poster)}
+                        value="decrease"
+                      >
+                        -
+                      </button>
+                      <p>{quantity}</p>
+                      <button
+                        className="increase-btn"
+                        onClick={(e) => cartQuantityHandler(e, poster)}
+                        value="increase"
+                      >
+                        +
+                      </button>
+                    </div>
                     <button
-                      className="decrease-btn"
-                      onClick={(e) => cartQuantityHandler(e, poster)}
-                      value="decrease"
+                      className="remove-cart-btn"
+                      onClick={() => removeFromCartBtnHandler(id)}
                     >
-                      -
-                    </button>
-                    <p>{quantity}</p>
-                    <button
-                      className="increase-btn"
-                      onClick={(e) => cartQuantityHandler(e, poster)}
-                      value="increase"
-                    >
-                      +
+                      Remove
                     </button>
                   </div>
-                  <button
-                    className="remove-cart-btn"
-                    onClick={() => removeFromCartBtnHandler(id)}
-                  >
-                    Remove
-                  </button>
-                </div>
-              </div>
-            );
-          })}
-        </div>
-        <div id="checkout-container">
-          <div>
-            {cart.map((poster) => {
-              return (
-                <div key={poster.id} className="checkout-posters">
-                  <p className="name-quantity">
-                    {poster.name} ({poster.quantity})
-                  </p>
-                  <p className="price-quantity">
-                    {(poster.quantity * poster.price).toFixed(2)}
-                  </p>
                 </div>
               );
             })}
           </div>
-          {total > 0 && (
-            <div id="total-price">
-              <p id="heading">Total</p>
-              <p id="price">
-                ₹<b>{total}</b>
-              </p>
+          <div id="checkout-container">
+            <div>
+              {cart.map((poster) => {
+                return (
+                  <div key={poster.id} className="checkout-posters">
+                    <p className="name-quantity">
+                      {poster.name} ({poster.quantity})
+                    </p>
+                    <p className="price-quantity">
+                      {(poster.quantity * poster.price).toFixed(2)}
+                    </p>
+                  </div>
+                );
+              })}
             </div>
-          )}
-          {cart.length > 0 && (
-            <Link to="/checkout">
-              <p id="checkout-link">Checkout</p>
-            </Link>
-          )}
+            {total > 0 && (
+              <div id="total-price">
+                <p id="heading">Total</p>
+                <p id="price">
+                  ₹<b>{total}</b>
+                </p>
+              </div>
+            )}
+            {cart.length > 0 && (
+              <Link to="/checkout">
+                <p id="checkout-link">Checkout</p>
+              </Link>
+            )}
+          </div>
         </div>
-      </div>
+      )}
     </>
   );
 }
