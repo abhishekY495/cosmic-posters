4fa7f5a3be67988c179776627eaf9ccc78b0b3d4
Abhishek <67749693+abhishekY495@users.noreply.github.com>
2023-06-06 14:18:00 +0530

    Added imageLink

--- src/data/categoryInfo.js
+++ src/data/categoryInfo.js
@@ -1,22 +1,30 @@
 export const categoryInfo = [
   {
     name: "Planet",
+    imageLink:
+      "https://res.cloudinary.com/dfuirkjxj/image/upload/w_800/v1685677259/category-images/planet_e1afbi.jpg",
     description:
       "Our solar system is home to 8 amazing planets, ranging from small rocky worlds to large and gaseous with extreme temperatures.",
   },
   {
-    name: "Moon",
-    description:
-      "Moons come in many shapes, sizes, and types. Few have atmospheres and even hidden oceans beneath their surfaces.",
-  },
-  {
     name: "Galaxy",
+    imageLink:
+      "https://res.cloudinary.com/dfuirkjxj/image/upload/w_800/v1685678874/category-images/galaxy_jcdo0z.jpg",
     description:
       "Galaxies are a huge collection of gas, dust, and billions of stars and their solar systems, all held together by gravity.",
   },
   {
-    name: "Nebula",
+    name: "Moon",
+    imageLink:
+      "https://res.cloudinary.com/dfuirkjxj/image/upload/w_800/v1685677235/category-images/moon_xvy6ta.jpg",
     description:
-      "A nebula is a giant cloud of dust and gas in space that come from the gas and dust thrown out by the explosion of a dying star.",
+      "Moons come in many shapes, sizes, and types. Few have atmospheres and even hidden oceans beneath their surfaces.",
+  },
+  {
+    name: "Nebula",
+    imageLink:
+      "https://res.cloudinary.com/dfuirkjxj/image/upload/w_800/v1685677235/category-images/nebula_x5oi7e.jpg",
+    description:
+      "A Nebula is a giant cloud of dust and gas in space that come from the gas and dust thrown out by the explosion of a dying star.",
   },
 ];
